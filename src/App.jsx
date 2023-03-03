6a2bb8295c75d4f0560f0e91d934e2a05d0df62e
Rizki Amanda Agustian <agustian32147@gmail.com>
2023-03-03 19:26:31 +0700

    menambahkan componen dan pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Profile from "./Components/profile";
 import Tombol from "./Components/Tombol";
+import Navbar from "./Components/Navbar";
+import Rating from "./Components/Rating";
+import HBIX from "./Asset/SULTAN.jpg"
 function App() {
   return (
-    <div>
-        <Tombol Title="Save"/>
-        <Tombol Title="Reset"/>
-        <Tombol Title="Restart"/>
-    </div>
-    
+    <>
+      <Navbar
+      />
+      <Profile
+        Asset = {HBIX}
+        Nama="Tian"
+        Umur="22"
+        Alamat="Bantul Yogyakarta"
+        Tinggi="180"
+        Berat="90"
+        Status="tes"
+      />
+      <Tombol />
+      <Rating
+      />
+    </>
   );
 }
 
--- src/Components/Tombol.jsx
+++ src/Components/Tombol.jsx
@@ -1,8 +1,10 @@
-function Tombol({Title}) {
-    return(
-        <>
-        <button>{Title}</button>
-        </>
-    )
+function Tombol({ Title }) {
+  return (
+    <>
+      <button className="bg-blue-500 text-white rounded pt-3 pb-3 pl-5 pr-5 mx-5">
+        {Title}Sumbit Here
+      </button>
+    </>
+  );
 }
 export default Tombol;
